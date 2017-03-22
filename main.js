1f68d5812c2055a9991069efc93183cd6511f8bd
sarah niemiec <sroseniemiec@gmail.com>
2017-03-22 17:56:38 -0400

    finished homework

--- main.js
+++ main.js
@@ -9,12 +9,12 @@ function Photo(filePath, location) {
   this.location = location
 }
 
-var londonPhoto = new Photo ("london.jpg", "London")
-var bigbenPhoto = new Photo ("bigben.jpg", "London")
-var westminsterabbyPhoto = new Photo ("westminsterabby.jpg", "London")
-var busPhoto = new Photo ("bus.jpg", "London")
-var streetPhoto = new Photo ("street.jpg", "London")
-var theQueenAndIPhoto = new Photo ("thequeenandi.jpg", "London")
+var santaFePhoto = new Photo ("santafe.jpg", "Santa Fe")
+var tentRocksPhoto = new Photo ("tentRocks.jpg", "Santa Fe")
+var stAntonioSpringsPhoto = new Photo ("stantoniosprings.jpg", "Santa Fe")
+var roofPhoto = new Photo ("roof.jpg", "Santa Fe")
+var doorPhoto = new Photo ("door.jpg", "Santa Fe")
+var cactusPhoto = new Photo ("cactus.jpg", "Santa Fe")
 var beachPhoto = new Photo ("beach.jpg", "St. Thomas")
 var palmtreePhoto = new Photo ("palmtree.jpg", "St. Thomas" )
 var dolphinsPhoto = new Photo ("dolphins.jpg", "St. Thomas")
@@ -37,21 +37,15 @@ function(photo) {
   this.photos.push(photo)
 }
 
-londonAlbum = new Album()
-londonAlbum.addingPhotos(londonPhoto)
-londonAlbum.addingPhotos(bigbenPhoto)
-londonAlbum.addingPhotos(westminsterabbyPhoto)
-londonAlbum.addingPhotos(busPhoto)
-londonAlbum.addingPhotos(streetPhoto)
-londonAlbum.addingPhotos(theQueenAndIPhoto)
+var santaFeAlbum = new Album()
+santaFeAlbum.addingPhotos(santaFePhoto)
+santaFeAlbum.addingPhotos(tentRocksPhoto)
+santaFeAlbum.addingPhotos(stAntonioSpringsPhoto)
+santaFeAlbum.addingPhotos(roofPhoto)
+santaFeAlbum.addingPhotos(doorPhoto)
+santaFeAlbum.addingPhotos(cactusPhoto)
 
-//listing all photos in londonAlbum
-console.log(londonAlbum);
-//accessing the first photo added to londonAlbum
-console.log(londonAlbum.photos[0])
-
-
-stThomasAlbum = new Album()
+var stThomasAlbum = new Album()
 stThomasAlbum.addingPhotos(beachPhoto)
 stThomasAlbum.addingPhotos(palmtreePhoto)
 stThomasAlbum.addingPhotos(dolphinsPhoto)
@@ -59,26 +53,124 @@ stThomasAlbum.addingPhotos(royalDaneMallPhoto)
 stThomasAlbum.addingPhotos(hikePhoto)
 stThomasAlbum.addingPhotos(iguanaPhoto)
 
-//listing all photos in stThomasAlbum
-console.log(stThomasAlbum);
-//accessing third photo added to stThomasAlbum
-console.log(stThomasAlbum.photos[2]);
 
-moonAlbum = new Album()
+var moonAlbum = new Album()
 moonAlbum.addingPhotos(moonwalkPhoto)
 moonAlbum.addingPhotos(flagPhoto)
 moonAlbum.addingPhotos(starsPhoto)
 moonAlbum.addingPhotos(milkywayPhoto)
 moonAlbum.addingPhotos(shootingStarPhoto)
 
-//listing all photos in moonAlbum
-console.log(moonAlbum);
-//accessing fourth photo added to moonAlbum
-console.log(moonAlbum.photos[3]);
+//accessing one photo in array
+Album.prototype.getPhoto = function (num) {
+  console.log(this.photos[num])
+}
 
+//listing items in the album
+Album.prototype.listItems = function () {
+for(var i = 0; i < this.photos.length; i++){
+  console.log(this.photos[i]);
+  }
+}
 
+// listing all photos in Albums
+moonAlbum.listItems()
+stThomasAlbum.listItems()
+santaFeAlbum.listItems()
 
-
+//accessing third item in moonAlbum
+moonAlbum.getPhoto(2)
+//accessing first item in stThomasAlbum
+stThomasAlbum.getPhoto(0)
+//accessing second item in londonAlbum
+santaFeAlbum.getPhoto(1)
 
 // Create a prototypical Person object. From this object, extend a Teacher object and a Student object. Each of these objects should have attributes and methods pertinent to what they describe. Also create a School object that should be able to store instances of students and teachers. Make sure to write code afterwards that creates instances of these objects to make sure that what you've written works well and you're able to store the necessary data in each object.
 // solution 2
+
+function Person(name, age, gender) {
+  this.name = name
+  this.age = age
+  this.gender = gender
+}
+
+Person.prototype.hello = function() {
+  console.log("Hello, my name is " + this.name + ". I am " + this.age + " years old. " + "I am " + this.gender + ".")
+}
+
+function Teacher(name, age, gender, subject) {
+  Person.call(this, name, age, gender)
+  this.subject = subject
+}
+
+Teacher.prototype = Object.create(Person.prototype)
+
+function Student(name, age, gender, interest) {
+  Person.call(this, name, age, gender)
+  this.interest = interest
+}
+
+Student.prototype = Object.create(Person.prototype)
+
+function Pet(name, species){
+this.name = name
+this.species = species
+}
+
+function School() {
+  this.people = []
+}
+
+School.prototype.addPerson = function(person) {
+  this.people.push(person)
+}
+
+var msFizzle = new Teacher("Ms Fizzle", 30, "female", "science")
+
+var drCarminaSkeledon = new Teacher("Dr Carmina Skeledon", 35, "female", "palentology")
+
+console.log(msFizzle);
+
+var keeshaFranklin = new Student("Keesha Franklin", 8, "female", "art")
+
+var carlosRamon = new Student("Carlos Ramon", 8, "male", "computers")
+
+var wandaLi = new Student("Wanda Li", 8, "female", "skiiing")
+
+var pheobeTerese = new Student("Pheobe Terese", 8, "male", "music")
+
+var lizTheChameleon = new Pet("Liz", "chameleon")
+console.log(lizTheChameleon);
+
+var magicSchoolBus = new School()
+
+magicSchoolBus.addPerson(msFizzle)
+magicSchoolBus.addPerson(drCarminaSkeledon)
+magicSchoolBus.addPerson(keeshaFranklin)
+magicSchoolBus.addPerson(carlosRamon)
+magicSchoolBus.addPerson(wandaLi)
+magicSchoolBus.addPerson(pheobeTerese)
+magicSchoolBus.addPerson(lizTheChameleon)
+
+School.prototype.listPeople = function() {
+for(var i = 0; i < this.people.length; i++){
+  console.log(this.people[i]);
+  }
+}
+
+School.prototype.getPerson = function (num) {
+  console.log(this.people[num])
+}
+
+//listing all people
+magicSchoolBus.listPeople()
+
+//list 3rd person
+magicSchoolBus.getPerson(2)
+
+console.log(msFizzle.hello())
+console.log(drCarminaSkeledon.hello());
+console.log(keeshaFranklin.hello())
+console.log(carlosRamon.hello())
+console.log(wandaLi.hello())
+console.log(pheobeTerese.hello())
